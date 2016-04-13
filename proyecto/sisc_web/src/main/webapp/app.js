0cd9cf10a61d0a83846a7737fe2658b0d9a81204
Erika Rivera <erivividamax@gmail.com>
2016-04-13 05:35:07 -0500

    Acercamiento a la plantilla con seguridad

---
+++ proyecto/sisc_web/src/main/webapp/app.js
@@ -0,0 +1,33 @@
+'use strict';
+// Declare app level module which depends on views, and components
+var app = angular.module('siscseguridad', ['ui.router','ngResource', 'ui.bootstrap','ngResource','ngGrid','app.utils']);
+
+app.config(['$stateProvider','$urlRouterProvider', function($stateProvider,$urlRouterProvider) {
+  $urlRouterProvider.otherwise("/home");
+  
+  $stateProvider.
+    state('home',{
+      url: '/home',
+      templateUrl: 'home.html'
+    })
+      
+    .state('usuarios',{
+      url:'/usuarios',
+      templateUrl: 'usuarios/usuarios.html',
+      controller: 'usuariosListController'
+    })
+  
+    .state('crearUsuario',{
+      url: '/crearUsuario',
+      templateUrl: 'usuarios/formularioUsuario.html',
+      controller: 'usuarioFormController'
+    })
+    
+    .state('modificarUsuario',{
+      url: '/modificarUsuario',
+      templateUrl: 'usuarios/formularioUsuario.html',
+      controller: 'usuarioFormController',
+      params : {'usuaUsua':null}
+    });
+    
+}]);
