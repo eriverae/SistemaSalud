28b0eb9c26e47e8938a11e77c45debc5332f0a92
Erika Rivera <erivividamax@gmail.com>
2016-04-16 08:43:30 -0500

    Verificacion de la contrasenia

--- proyecto/sisc_web/src/main/webapp/usuarios/usuarioFormController.js
+++ proyecto/sisc_web/src/main/webapp/usuarios/usuarioFormController.js
@@ -77,3 +77,15 @@ app.controller('usuarioFormController', function ($scope, $rootScope, $statePara
   };
   
 });
+
+app.directive('wjValidationError', function () {
+  return {
+    require: 'ngModel',
+    link: function (scope, elm, attrs, ctl) {
+      scope.$watch(attrs['wjValidationError'], function (errorMsg) {
+        elm[0].setCustomValidity(errorMsg);
+        ctl.$setValidity('wjValidationError', errorMsg ? false : true);
+      });
+    }
+  };
+});
